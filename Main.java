db86e8e37574535a74edd4e83d36a4ff98b9b220
Dinnikurnia <dinnikurnianti@gmail.com>
2021-10-22 16:00:38 +0700

    Dinni Game Group 5

---
+++ Main.java
@@ -0,0 +1,86 @@
+class Main {
+    public static void main(String[] args) {
+      Hero hero = new Hero("Dinni Kurnianti", 20);
+  
+      hero.player();
+  
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+
+      hero.run();
+      hero.run();
+      
+      hero.eat();
+      hero.eat();
+      hero.eat();
+
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+      hero.walk();
+
+      hero.drink();
+      hero.drink();
+      hero.drink();
+      hero.drink();
+      hero.drink();
+
+      hero.jump();
+      hero.jump();
+      hero.jump();
+
+      hero.sit();
+      hero.sit();
+      hero.sit();
+      hero.sit();
+      hero.sit();
+
+      hero.attack();
+      hero.attack();
+      hero.attack();
+      hero.attack();
+      hero.attack();
+      hero.attack();
+      
+      /*
+      Group 5: walk 10x, run 2x, eat 3x,  walk 30x, drink 5x, jump 3x, sit 5x, attack 6x
+      */
+      
+      hero.player();
+   
+    }
+  }
